60baad1683e5eaddbe852d8d71bd9f23c81a1bbb
pranitasirsat152-creator <pranitasirsat152@gmail.com>
2026-07-24 22:43:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import pandas as pd
 import joblib
 import numpy as np
 
+
 model = joblib.load("LR model.pkl")
 scaler = joblib.load("Scaler..pkl")
-model = joblib.load("columns..pkl")
+model_columns = joblib.load("columns..pkl") 
 
-st.title("Car Price Prediction App")
+st.title("🚗 Car Price Prediction App")
 
 model_name = st.selectbox("Model",["Fiesta","Focus","EcoSport","Kuga","Mondeo"])
 year = st.number_input("Year",2000,2025,2020)
@@ -19,6 +20,7 @@ transmission = st.selectbox("Transmission",["Manual","Automatic","Semi-Auto"])
 fuelType = st.selectbox("Fuel Type",["Petrol","Diesel","Hybrid","Electric"])
 
 if st.button("Predict Price"):
+    
     input_data = pd.DataFrame([{
         "model":model_name,
         "year":year,
@@ -30,10 +32,16 @@ if st.button("Predict Price"):
         "fuelType":fuelType
     }])
 
+    
     input_encoded = pd.get_dummies(input_data)
-    model_columns = joblib.load("columns..pkl")  
-    input_encoded = input_encoded.reindex(columns = model_columns , fill_value=0)
+
+
+    input_encoded = input_encoded.reindex(columns = model_columns, fill_value=0)
+
+
     input_scaled = scaler.transform(input_encoded)
+
+    
     prediction = model.predict(input_scaled)
 
-    st.success("Predicted Price: £ {prediction[0]:.2f}")
+    st.success(f"Predicted Price: £{prediction[0]:.2f}")
